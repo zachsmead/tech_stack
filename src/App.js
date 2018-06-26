883c9825a4d24147a044fdbe02c7bbad471de370
= <zrsmead@gmail.com>
2018-06-26 14:05:58 -0700

    styling on button

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import { View } from 'react-native';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import reducers from './reducers';
+import { Header } from './components/common';
 
 const App = () => {
   return (
     <Provider store={createStore(reducers)}>
+      <Header headerText='Tech Stack' />
       <View />
     <Provider />
   );
---
+++ src/components/common/index.js
@@ -0,0 +1,10 @@
+export * from './Button';
+export * from './Card';
+export * from './CardSection';
+export * from './Header';
+export * from './Input';
+export * from './Spinner';
+
+// The above pattern both imports and exports components.
+// When using this pattern, you can't use the 'default'
+// keyword in the files you're importing.
