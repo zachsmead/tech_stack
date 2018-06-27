df3095f4e98300c6b32a5bdd059e78dc54909d56
= <zrsmead@gmail.com>
2018-06-27 00:35:11 -0700

    added actions, reducers, etc

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,8 @@
 import { combineReducers } from 'redux';
-import { LibraryReducer } from './LibraryReducer';
+import LibraryReducer from './LibraryReducer';
+import SelectionReducer from './LibraryReducer';
 
 export default combineReducers({
-  libraries: LibraryReducer
+  libraries: LibraryReducer,
+  selectedLibraryId: SelectionReducer
 });
